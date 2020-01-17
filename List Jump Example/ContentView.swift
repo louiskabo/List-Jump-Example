afda4efb05d696c0d502e59fb6d9f16e200cc53f
macbook <louis@kaboserv.com>
2020-01-17 16:18:36 -0500

    minor correction

--- List Jump Example/ContentView.swift
+++ List Jump Example/ContentView.swift
@@ -100,7 +100,7 @@ struct ListItemForOrder: Identifiable {
     }
 }
 
-struct SubListNJAll_Previews: PreviewProvider {
+struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
